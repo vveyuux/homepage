65ab53bd033b9c5c2d2889312fc40f56d9c25cb5
Veerayuth Bussararungsee <vbussararungsee@gmail.com>
2024-07-28 00:21:41 +0700

    Add profile pic

--- src/components/sections/Info.tsx
+++ src/components/sections/Info.tsx
@@ -5,7 +5,17 @@ import LinkIcon from "../icon/LinkIcon";
 const Info: React.FC<InfoPropsInterface> = ({ fullname, position, status }) => {
     return (
         <>
-            <h1 className="text-4xl font-bold tracking-tight text-text sm:text-5xl">{fullname ? fullname : "Full Name Here"}</h1>
+            <div className="lg:mb-6 group relative grid lg:place-items-center gap-4 sm:grid-cols-8 sm:gap-8 md:gap-4">
+                <img
+                    loading="lazy"
+                    className="sm:order-1 sm:col-span-2 sm:translate-y-1 info-img hidden lg:block"
+                    src="src/assets/images/me.jpg"
+                    alt="me"
+                />
+                <h1 className="sm:order-2 sm:col-span-6 text-5xl font-bold tracking-tight text-text md:text-4xl">
+                    {fullname ? fullname : "Full Name Here"}
+                </h1>
+            </div>
             <h2 className="mt-3 text-lg font-medium tracking-tight text-text sm:text-xl">{position ? position : "Position Here"}</h2>
             <p className="mt-4 max-w-xs leading-normal">
                 {status ? status : "Status Here"}
