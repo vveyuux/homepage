768c7cf3d4892e826327569a79f6735df1405203
Veerayuth Bussararungsee <vbussararungsee@gmail.com>
2024-07-28 00:21:29 +0700

    Delete some properties

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -46,11 +46,9 @@ const Projects: React.FC<ProjectPropsInterface> = ({ projects }) => {
                                 </div>
                                 <img
                                     alt={proj.imgAlt}
-                                    loading="lazy"
-                                    width="200"
-                                    height="48"
-                                    decoding="async"
-                                    data-nimg="1"
+                                    loading={"lazy"}
+                                    width={"200"}
+                                    height={"48"}
                                     className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
                                     style={{ color: "transparent" }}
                                     src={proj.imgPath}
